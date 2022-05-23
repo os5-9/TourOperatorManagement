be180e186ac8690e7265713fb0b077f21b25acde
Ольга Шапинская <os.5-9@yandex.ru>
2022-05-24 01:22:21 +0400

    add auth logic

--- AuthWindow.xaml.cs
+++ AuthWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TourOperatorManagement.Models;
 
 namespace TourOperatorManagement
 {
@@ -19,6 +20,7 @@ namespace TourOperatorManagement
     /// </summary>
     public partial class AuthWindow : Window
     {
+        private AgencyModel model = new AgencyModel();
         public AuthWindow()
         {
             InitializeComponent();
@@ -26,17 +28,48 @@ namespace TourOperatorManagement
 
         private void btnEnter_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!string.IsNullOrEmpty(tbLogin.Text) && !string.IsNullOrEmpty(tbPassword.Password))
+            {
+                string login = tbLogin.Text;
+                string password = tbPassword.Password;
+                if (model.Operators.Where(x => x.Password == password && x.Login == login).Any())
+                {
+                    var person = model.Operators.Where(x => x.Password == password && x.Login == login).FirstOrDefault();
+                    if (person.IsDenied == 1)
+                    {
+                        MainWindow window = new MainWindow();
+                        window.Show();
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ваш аккаунт на верификации, попробуйте позже");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Заполните все поля");
+            }
         }
 
         private void tbGoToRegister_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            RegisterWindow register = new RegisterWindow();
+            register.Show();
+            this.Close();
         }
 
         private void Password_KeyDown(object sender, KeyEventArgs e)
         {
-
+            if (e.Key == Key.Enter)
+            {
+                btnEnter_Click(sender, e);
+            }
         }
     }
 }
